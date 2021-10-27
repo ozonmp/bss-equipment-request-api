e1d5bcfae8ea23ae349522e796e6d711838d9f1b
Valeria Yurinskaya <valeriayurinskaya@gmail.com>
2021-10-27 14:44:50 +0300

    [Task-2] - Add description how we can support multiple types of events given the correct order

--- internal/app/repo/event.go
+++ internal/app/repo/event.go
@@ -5,6 +5,22 @@ import (
 )
 
 type EventRepo interface {
+
+	/**
+	If we want to support multiple types of events given the correct order
+	we can use this query inside Lock function to get each time only the first and not locked events of entity by its type
+	WITH added_row_number AS (
+	  SELECT
+		*,
+		ROW_NUMBER() OVER(PARTITION BY entity_id order by type, id) AS row_number
+	  FROM events
+	)
+	SELECT
+	  *
+	FROM added_row_number
+	WHERE row_number = 1;
+	*/
+
 	Lock(n uint64) ([]model.EquipmentRequestEvent, error)
 	Unlock(eventIDs []uint64) error
 
