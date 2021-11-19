c200041f68ff8c0385900ed11b8b4ffd1414bb1c
Valeria Yurinskaya <valeriayurinskaya@gmail.com>
2021-11-19 20:51:33 +0300

    [Task-5] - Add hey

---
+++ example/hey/hey-remove-equipment-request.sh
@@ -0,0 +1,1 @@
+for run in {1..50}; do a=$(( ( RANDOM % 10000 )  + 1 )) ;hey -n 5 -c 2 -m POST -H 'accept: application/json' -H 'Content-Type: application/json' -d "{ \"equipment_request_id\": "${a}"}" http://0.0.0.0:8083/api/v1/equipment_requests/remove ; done
---
+++ example/hey/hey-update-status-equipment-request.sh
@@ -0,0 +1,1 @@
+for run in {1..50}; do a=$(( ( RANDOM % 10000 )  + 1 )) ; b=$(( ( RANDOM % 6 )  + 1 )) ;hey -n 5 -c 2 -m POST -H 'accept: application/json' -H 'Content-Type: application/json' -d "{ \"equipment_request_id\": "${a}", \"equipmentRequestStatus\": "${b}"}" http://0.0.0.0:8083/api/v1/update/status ; done
